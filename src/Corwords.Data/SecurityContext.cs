5d77398c25043817ba6e0467e34535d9871962fd
Jason Gaylord <jason.gaylord@guard.com>
2016-09-02 15:40:42 -0400

    Added the ApplicationUser object to the Data project.

---
+++ src/Corwords.Data/SecurityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corwords.Data
+{
+    public class SecurityContext : IdentityDbContext<ApplicationUser>
+    {
+        public SecurityContext(DbContextOptions<SecurityContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}

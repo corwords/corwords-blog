7f8f75aa99ce24e66fbf63f70234b00c54096d8b
Jason Gaylord <jason.gaylord@guard.com>
2016-09-02 16:23:36 -0400

    Missed adding in the additional DBSets. Updated the Core project.json too.

--- src/Corwords.Data/_Contexts/CorwordsContext.cs
+++ src/Corwords.Data/_Contexts/CorwordsContext.cs
@@ -11,5 +11,8 @@ namespace Corwords.Data
         public DbSet<Blog.Post> Posts { get; set; }
         public DbSet<Blog.Category> Categories { get; set; }
         public DbSet<Blog.MediaObject> MediaObjects { get; set; }
+        public DbSet<Blog.MediaObjectInfo> MediaObjectInfos { get; set; }
+        public DbSet<Blog.Enclosure> Enclosures { get; set; }
+        public DbSet<Blog.Source> Sources { get; set; }
     }
 }

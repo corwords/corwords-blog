51bf7208bd38d6011a0aeaa43a75bafa8c7a797e
Jason Gaylord <jason.gaylord@guard.com>
2016-09-12 16:28:00 -0400

    Possibly init a user?

---
+++ src/Corwords.Core/Security/Init.cs
@@ -0,0 +1,21 @@
+﻿using Corwords.Data.Security;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corwords.Core.Security
+{
+    public class Init
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public void AddAdminUser()
+        {
+            var user = new ApplicationUser { };
+
+            _userManager.CreateAsync(new ApplicationUser() { });
+        }
+    }
+}

3cbfe161673f928175c02f317644b794a1c93981
Jason Gaylord <jason.gaylord@guard.com>
2016-10-12 15:47:46 -0400

    Added the firstruncontraint.

--- src/Corwords.Core/MVC/FirstRunContraint.cs
+++ src/Corwords.Core/MVC/FirstRunContraint.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Corwords.Core.Config;
 
 namespace Corwords.Core.MVC
 {

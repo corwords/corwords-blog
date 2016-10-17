c544c001ca9980795d80220cba0d5a29115a791f
Jason Gaylord <jason.gaylord@guard.com>
2016-10-17 13:08:34 -0400

    Updated FirstRunOptions to make sure that the first run values included email.

--- src/Corwords.Core/Config/FirstRunOptions.cs
+++ src/Corwords.Core/Config/FirstRunOptions.cs
@@ -9,6 +9,7 @@ namespace Corwords.Core.Config
     public class FirstRunOptions
     {
         public bool FirstRunEnabled { get; set; }
+        public string AdminEmailAddress { get; set; }
         public string AdminUsername { get; set; }
         public string AdminPassword { get; set; }
     }

8658b6c9967bd7374d4bc5f442fcab2d9a9d166e
Jason Gaylord <jason@jasongaylord.com>
2016-10-20 13:47:00 -0400

    Added a blog manager and a method to create a blog.

---
+++ src/Corwords.Core/Content/Blog/BlogManager.cs
@@ -0,0 +1,37 @@
+﻿using Corwords.Data;
+using Corwords.Data.Blog;
+using System.Linq;
+
+namespace Corwords.Core.Content.Blog
+{
+    public class BlogManager
+    {
+        private CorwordsContext _context;
+
+        public BlogManager(CorwordsContext context)
+        {
+            _context = context;
+        }
+
+        public TransactionStatus CreateBlog(string name, string url)
+        {
+            var status = new TransactionStatus();
+
+            var sameBlogName = _context.Blogs.Any(a => a.Name == name);
+            if (sameBlogName)
+                status.AddFailMessage("A blog with this name already exists.");
+
+            var sameBlogUrl = _context.Blogs.Any(a => a.Url == url);
+            if (sameBlogUrl)
+                status.AddFailMessage("A blog with this URL already exists.");
+
+            if (status.Success)
+            {
+                var blog = new IndividualBlog() { Name = name, Url = url };
+                _context.Blogs.Add(blog);
+            }
+
+            return status;
+        }
+    }
+}

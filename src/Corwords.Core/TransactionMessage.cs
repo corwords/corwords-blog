cd8722844269366384ef8ca41420e85e76e38e35
Jason Gaylord <jason@jasongaylord.com>
2016-10-20 12:42:27 -0400

    Added core classes for returning transactional messages.

---
+++ src/Corwords.Core/TransactionMessage.cs
@@ -0,0 +1,37 @@
+﻿namespace Corwords.Core
+{
+    public class TransactionMessage
+    {
+        public string MessageKey { get; set; }
+        public string Message { get; set; }
+        public bool DisplayMessage { get; set; }
+
+        public TransactionMessage()
+        {
+            MessageKey = "default";
+            Message = "";
+            DisplayMessage = true;
+        }
+
+        public TransactionMessage(string message)
+        {
+            MessageKey = "default";
+            Message = message;
+            DisplayMessage = true;
+        }
+
+        public TransactionMessage(string key, string message)
+        {
+            MessageKey = key;
+            Message = message;
+            DisplayMessage = true;
+        }
+
+        public TransactionMessage(string message, bool display)
+        {
+            MessageKey = "default";
+            Message = message;
+            DisplayMessage = display;
+        }
+    }
+}
---
+++ src/Corwords.Core/TransactionStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Corwords.Core
+{
+    public class TransactionStatus
+    {
+        public bool Success { get; set; }
+        public List<TransactionMessage> Messages { get; set; }
+
+        public TransactionStatus()
+        {
+            Messages = new List<TransactionMessage>();
+        }
+
+        public TransactionStatus(bool success, string message)
+        {
+            Success = success;
+            Messages = new List<TransactionMessage> { new TransactionMessage(message) };
+        }
+    }
+}

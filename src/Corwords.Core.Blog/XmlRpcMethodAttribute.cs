4855d7876c0bc9086827fa0129ff66871045fca8
Jason Gaylord <jason.gaylord@guard.com>
2016-10-21 12:59:47 -0400

    Added XmlRpcMethodAttribute for metaweblog connectivity

---
+++ src/Corwords.Core.Blog/XmlRpcMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Corwords.Core.Blog
+{
+    public class XmlRpcMethodAttribute : Attribute
+    {
+        public XmlRpcMethodAttribute(string methodName)
+        {
+            MethodName = methodName;
+        }
+
+        public string MethodName { get; set; }
+    }
+}

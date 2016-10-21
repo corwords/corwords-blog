4c044420223c32b84438cfbd494860c3e3cd3c8d
Jason Gaylord <jason.gaylord@guard.com>
2016-10-21 13:00:17 -0400

    Added the rest of the BlogPost fields to BlogPost.

--- src/Corwords.Core.Blog.EntityFrameworkCore/BlogPost.cs
+++ src/Corwords.Core.Blog.EntityFrameworkCore/BlogPost.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Corwords.Core.Blog.EntityFrameworkCore
 {
     public class BlogPost<TKey> where TKey : IEquatable<TKey>
     {
         public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Content { get; set; }
+        public virtual string Slug { get; set; }
+        public virtual string Author { get; set; }
+        public virtual string AuthorUsername { get; set; }
+        public virtual DateTime DateCreated { get; set; }
+        public virtual DateTime DateUpdate { get; set; }
+
         public virtual TKey BlogId { get; set; }
+        public virtual ICollection<BlogTag<TKey>> Tags { get; set; }
     }
 }

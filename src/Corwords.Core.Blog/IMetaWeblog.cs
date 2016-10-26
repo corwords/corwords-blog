4c4b8d7ec0d43ded5fd882753427d1a0858600fe
Jason Gaylord <jason@jasongaylord.com>
2016-10-26 00:42:36 -0400

    Moved most of IBlogService to IMetaWeblog so the blog service can be extended.

---
+++ src/Corwords.Core.Blog/IMetaWeblog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Corwords.Core.Blog
+{
+    public interface IMetaWeblog<TBlog, TBlogPost, TPostTag> : IDisposable
+        where TBlog : class, IBlog<TBlog, TBlogPost, TPostTag>
+        where TBlogPost : class, IBlogPost<TBlog, TBlogPost, TPostTag>
+        where TPostTag : class, IPostTag<TBlog, TBlogPost, TPostTag>
+    {
+        IUserInfo GetUserInfo(string key, string username, string password);
+        IBlog<TBlog, TBlogPost, TPostTag>[] GetUsersBlogs(string key, string username, string password);
+
+        IBlogPost<TBlog, TBlogPost, TPostTag> GetPost(string postid, string username, string password);
+        IBlogPost<TBlog, TBlogPost, TPostTag>[] GetRecentPosts(string blogid, string username, string password, int numberOfPosts);
+
+        string AddPost(string blogid, string username, string password, IBlogPost<TBlog, TBlogPost, TPostTag> post, bool publish);
+        bool DeletePost(string key, string postid, string username, string password, bool publish);
+        bool EditPost(string postid, string username, string password, IBlogPost<TBlog, TBlogPost, TPostTag> post, bool publish);
+
+        ITag<TBlog, TBlogPost, TPostTag>[] GetCategories(string blogid, string username, string password);
+        int AddCategory(string key, string username, string password, ITag<TBlog, TBlogPost, TPostTag> category);
+
+        IMediaObjectInfo NewMediaObject(string blogid, string username, string password, IMediaObject mediaObject);
+    }
+}

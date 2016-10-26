ff32061cd041be307274667617bc910069708b1d
Jason Gaylord <jason@jasongaylord.com>
2016-10-25 22:53:59 -0400

    Adding initial Corwords migration

---
+++ src/Corwords/Migrations/20161026025304_InitialCorwordsContext.Designer.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Corwords.Data;
+
+namespace Corwords.Migrations
+{
+    [DbContext(typeof(CorwordsContext))]
+    [Migration("20161026025304_InitialCorwordsContext")]
+    partial class InitialCorwordsContext
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.Blog", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<string>("Url")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Corwords_Blogs");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.BlogPost", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Author")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<string>("AuthorUsername")
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<int?>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<DateTime>("DateCreated");
+
+                    b.Property<DateTime>("DateUpdate");
+
+                    b.Property<string>("Slug")
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Corwords_BlogPosts");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.Enclosure", b =>
+                {
+                    b.Property<int>("EnclosureId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Length");
+
+                    b.Property<string>("Type");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("EnclosureId");
+
+                    b.ToTable("Corwords_BlogEnclosures");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.MediaObject", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Bits");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Type");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Corwords_BlogMediaObjects");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.MediaObjectInfo", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Corwords_BlogMediaObjectInfos");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.PostTag", b =>
+                {
+                    b.Property<int>("PostTagId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("PostId");
+
+                    b.Property<int>("TagId");
+
+                    b.HasKey("PostTagId");
+
+                    b.HasIndex("PostId");
+
+                    b.HasIndex("TagId");
+
+                    b.ToTable("Corwords_BlogPostTags");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.Source", b =>
+                {
+                    b.Property<int>("SourceId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("SourceId");
+
+                    b.ToTable("Corwords_BlogSources");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.Tag", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DateCreated");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Corwords_BlogTags");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.BlogPost", b =>
+                {
+                    b.HasOne("Corwords.Core.Blog.EntityFrameworkCore.Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId");
+                });
+
+            modelBuilder.Entity("Corwords.Core.Blog.EntityFrameworkCore.PostTag", b =>
+                {
+                    b.HasOne("Corwords.Core.Blog.EntityFrameworkCore.BlogPost", "Post")
+                        .WithMany("PostTags")
+                        .HasForeignKey("PostId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Corwords.Core.Blog.EntityFrameworkCore.Tag", "Tag")
+                        .WithMany("PostTags")
+                        .HasForeignKey("TagId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ src/Corwords/Migrations/20161026025304_InitialCorwordsContext.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Corwords.Migrations
+{
+    public partial class InitialCorwordsContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Corwords_Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Url = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogEnclosures",
+                columns: table => new
+                {
+                    EnclosureId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Length = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogEnclosures", x => x.EnclosureId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogMediaObjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Bits = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogMediaObjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogMediaObjectInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogMediaObjectInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogSources",
+                columns: table => new
+                {
+                    SourceId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogSources", x => x.SourceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(maxLength: 255, nullable: false),
+                    AuthorUsername = table.Column<string>(maxLength: 255, nullable: true),
+                    BlogId = table.Column<int>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdate = table.Column<DateTime>(nullable: false),
+                    Slug = table.Column<string>(maxLength: 255, nullable: true),
+                    Title = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogPosts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Corwords_BlogPosts_Corwords_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Corwords_Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corwords_BlogPostTags",
+                columns: table => new
+                {
+                    PostTagId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PostId = table.Column<int>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corwords_BlogPostTags", x => x.PostTagId);
+                    table.ForeignKey(
+                        name: "FK_Corwords_BlogPostTags_Corwords_BlogPosts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Corwords_BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Corwords_BlogPostTags_Corwords_BlogTags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Corwords_BlogTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Corwords_BlogPosts_BlogId",
+                table: "Corwords_BlogPosts",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Corwords_BlogPostTags_PostId",
+                table: "Corwords_BlogPostTags",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Corwords_BlogPostTags_TagId",
+                table: "Corwords_BlogPostTags",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogEnclosures");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogMediaObjects");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogMediaObjectInfos");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogPostTags");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogSources");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogPosts");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_BlogTags");
+
+            migrationBuilder.DropTable(
+                name: "Corwords_Blogs");
+        }
+    }
+}

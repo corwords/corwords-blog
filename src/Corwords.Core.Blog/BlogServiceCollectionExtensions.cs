d23f735762c7e804407667145b781b5b414edd8a
Jason Gaylord <jason@jasongaylord.com>
2016-10-27 06:46:10 -0400

    Adding IBlogUserService to the AddBlogging service extension.

--- src/Corwords.Core.Blog/BlogServiceCollectionExtensions.cs
+++ src/Corwords.Core.Blog/BlogServiceCollectionExtensions.cs
@@ -13,15 +13,17 @@ namespace Corwords.Core.Blog
             return builder.UseMiddleware<MetaWeblogMiddleware<TBlog, TBlogPost, TPostTag>>(path);
         }
 
-        public static BlogBuilder AddBlogging<TBlogService, TBlog, TBlogPost, TPostTag>(this IServiceCollection services)
+        public static BlogBuilder AddBlogging<TBlogService, TBlogUserService, TBlog, TBlogPost, TPostTag>(this IServiceCollection services)
             where TBlogService : class, IBlogService<TBlog, TBlogPost, TPostTag>
+            where TBlogUserService : class, IBlogUserService
             where TBlog : class, IBlog<TBlog, TBlogPost, TPostTag>
             where TBlogPost : class, IBlogPost<TBlog, TBlogPost, TPostTag>
             where TPostTag : class, IPostTag<TBlog, TBlogPost, TPostTag>
         {
             services.AddScoped<IBlogService<TBlog, TBlogPost, TPostTag>, TBlogService>();
+            services.AddScoped<IBlogUserService>();
             services.AddScoped<MetaWeblogService<TBlog, TBlogPost, TPostTag>>();
-            return new BlogBuilder(typeof(TBlogService), services);
+            return new BlogBuilder(typeof(TBlogService), typeof(TBlogUserService), services);
         }
     }
 }
---
+++ src/Corwords.Core.Blog/IBlogUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corwords.Core.Blog
+{
+    public interface IBlogUserService
+    {
+    }
+}

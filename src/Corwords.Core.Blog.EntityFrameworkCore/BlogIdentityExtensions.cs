463165baabf7bfda1c4ae5638b77ef884a33ea4d
Jason Gaylord <jason@jasongaylord.com>
2016-10-27 06:52:04 -0400

    Implement the new IBlogUserService

---
+++ src/Corwords.Core.Blog.EntityFrameworkCore/BlogIdentityExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corwords.Core.Blog.EntityFrameworkCore
+{
+    public static class BlogIdentityExtensions
+    {
+        public static BlogBuilder AddIdentity<TContext, TUser, TRole>(this BlogBuilder builder)
+            where TUser : class
+            where TRole : class
+            where TContext : DbContext
+        {
+            builder.Services.TryAdd(GetDefaultServices(builder.BlogUserType, typeof(TContext), typeof(TUser), typeof(TRole)));
+            return builder;
+        }
+
+        private static IServiceCollection GetDefaultServices(Type blogUserType, Type contextType, Type userType, Type roleType)
+        {
+            var blogUserServiceType = typeof(BlogUserService).MakeGenericType(blogUserType, contextType);
+            var services = new ServiceCollection();
+            services.AddScoped(typeof(IBlogUserService).MakeGenericType(blogUserType), blogUserServiceType);
+            return services;
+        }
+    }
+}

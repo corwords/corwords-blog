cc30f2ccd7812b28ed242f1a622647cccd2f11b8
Jason Gaylord <jason@jasongaylord.com>
2016-10-26 20:55:44 -0400

    Adding password as a SecureString to userinfo

--- src/Corwords.Core.Blog/IUserInfo.cs
+++ src/Corwords.Core.Blog/IUserInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Threading.Tasks;
 
 namespace Corwords.Core.Blog
@@ -9,6 +10,7 @@ namespace Corwords.Core.Blog
     {
         string Id { get; set; }
         string Username { get; set; }
+        SecureString Password { get; set; }
         string FirstName { get; set; }
         string LastName { get; set; }
         string EmailAddress { get; set; }

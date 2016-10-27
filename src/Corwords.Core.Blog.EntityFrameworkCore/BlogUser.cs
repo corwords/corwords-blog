5402c20db96cb1f7e3c4bc5a52513e0fd9da639f
Jason Gaylord <jason@jasongaylord.com>
2016-10-26 21:04:32 -0400

    Updated BlogUser to include Password

--- src/Corwords.Core.Blog.EntityFrameworkCore/BlogUser.cs
+++ src/Corwords.Core.Blog.EntityFrameworkCore/BlogUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Threading.Tasks;
 
 namespace Corwords.Core.Blog.EntityFrameworkCore
@@ -9,6 +10,7 @@ namespace Corwords.Core.Blog.EntityFrameworkCore
     {
         public virtual string Id { get; set; }
         public virtual string Username { get; set; }
+        public virtual SecureString Password { get; set; }
         public virtual string FirstName { get; set; }
         public virtual string LastName { get; set; }
         public virtual string EmailAddress { get; set; }

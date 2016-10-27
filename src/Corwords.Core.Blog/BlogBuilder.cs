55fa93412a547ae9dd32668b20d39da56d84fa08
Jason Gaylord <jason@jasongaylord.com>
2016-10-27 06:31:52 -0400

    Adding new type to BlogBuilder to inject a BlogUserService

--- src/Corwords.Core.Blog/BlogBuilder.cs
+++ src/Corwords.Core.Blog/BlogBuilder.cs
@@ -6,11 +6,13 @@ namespace Corwords.Core.Blog
     public class BlogBuilder
     {
         public Type BlogType { get; private set; }
+        public Type BlogUserType { get; private set; }
         public IServiceCollection Services { get; private set; }
 
-        public BlogBuilder(Type blog, IServiceCollection services)
+        public BlogBuilder(Type blog, Type blogUser, IServiceCollection services)
         {
             BlogType = blog;
+            BlogUserType = blogUser;
             Services = services;
         }
     }

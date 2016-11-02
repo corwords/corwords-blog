af21621354b3158a01e67afcf6c1f8cc23462378
Jason Gaylord <jason@jasongaylord.com>
2016-11-02 16:21:01 -0400

    Updated the BlogUserService to accept TUserManager and TRoleManager

--- src/Corwords.Core.Blog.EntityFrameworkCore/BlogUserService.cs
+++ src/Corwords.Core.Blog.EntityFrameworkCore/BlogUserService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Corwords.Core.Blog.EntityFrameworkCore
 {
-    public class BlogUserService : IBlogUserService
+    public class BlogUserService : IBlogUserService<Microsoft.AspNetCore.Identity.UserManager, Microsoft.AspNetCore.Identity.RoleManager>
     {
     }
 }
